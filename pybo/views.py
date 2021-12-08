af6eda64ced1441f11172afe00ce90a5c0695e82
farrar142 <gksdjf1690@gmail.com>
2021-12-08 13:49:05 +0900

    academy

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
-from .models import Question
+from .models import Question,Answer
+from .forms import QuestionForm,AnswerForm
 
 # Create your views here.
 def main_index(request):
@@ -18,4 +20,70 @@ def index(request):
 def detail(request,question_id):
     question = get_object_or_404(Question, pk=question_id)#id값으로 필터해서 보여줌
     context = {'question':question}
+
     return render(request,'pybo/question_detail.html',context)
+
+def question_create(request):
+    '''
+    pybo 질문 등록
+    '''
+    if request.method =='POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html',context)
+
+def question_delete(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)#id값으로 필터해서 보여줌
+    #question.delete()#게시물 삭제 후
+    question_list = Question.objects.order_by('-create_date')#다시 담아서
+    context = {'question_list':question_list}#컨텍스트에 담음.
+
+    return render(request, 'pybo/question_list.html', context)
+#
+def answer_create(request,question_id):
+    '''
+    pybo 답변 등록
+    '''
+    question = get_object_or_404(Question,pk=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail',question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question':question,'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_delete(request,question_id):
+    '''
+    pybo 답변 삭제
+    '''#
+    question = get_object_or_404(Question,pk=question_id)
+    for i in question.answer_set.all():
+        if question==i.question:
+            i.delete()
+            break
+    return redirect('pybo:detail', question_id=question_id)
+
+
+def answer_revise(request,question_id):
+    '''
+    pybo 답변 삭제
+    '''
+    question = get_object_or_404(Question,pk=question_id)
+    for i in question.answer_set.all():
+        if question==i.question:
+            i.delete()
+            break
+    return redirect('pybo:detail', question_id=question_id)
--- site2/settings.py
+++ site2/settings.py
@@ -15,7 +15,6 @@ from secured import import_database as _database
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -68,6 +67,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
 ]
 
 WSGI_APPLICATION = 'site2.wsgi.application'
