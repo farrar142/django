aac842d885d525f6249ddd3b1e6d500ca88e6d24
farrar142 <gksdjf1690@gmail.com>
2021-12-15 01:31:18 +0900

    asds

--- run_python.sh
+++ run_python.sh
@@ -1,3 +1,3 @@
-sudo python manage.py makemigrations
-sudo python manage.py migrate
-sudo python manage.py runserver 0.0.0.0:8000
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8000
