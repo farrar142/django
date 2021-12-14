934f524777fa8057d518098579e61d4ea2a38439
farrar142 <gksdjf1690@gmail.com>
2021-12-15 00:45:10 +0900

    asdsad

--- secured.py
+++ secured.py
@@ -1,16 +1,6 @@
 import_database = {
     'default': {
-    	# django db를 mysql로 사용하겠음
-        'ENGINE': 'django.db.backends.mysql',
-        # DB 이름 지어주기
-        'NAME': 'djangodatabase',
-        # 사용자 유저 계정 생성 후 입력하기
-        'USER': 'root',
-        # 사용자 비밀번호 생성 후 입력하기
-        'PASSWORD': '1234',
-        # default host인 localhost
-        'HOST': 'db',
-        # MySQL default 포트 번호
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
