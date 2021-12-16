7c0682548750b1f0505b631f62d71209a21033a6
farrar142 <gksdjf1690@gmail.com>
2021-12-16 23:15:05 +0900

    asd

--- distribute.sh
+++ distribute.sh
@@ -2,7 +2,7 @@
 # 2021. 12. 14 Farrar142
 ##
 docker_username="farrar142"
-web_image_name="ec2_cicd_web"
+web_image_name="sampleproject_web"
 web_container_name="web"
 version="1"
 port=8000 # Default django Port: 8000
