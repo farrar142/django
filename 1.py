07840aaa766008916d6255685edeb16286e5537e
farrar142 <gksdjf1690@gmail.com>
2021-12-17 17:47:40 +0900

    python script

--- 1.py
+++ 1.py
@@ -1,13 +1,23 @@
+# -*- coding: UTF-8 -*-
 import os
-a = os.popen('docker images')
-b = a.read().split("\n")
-column = b.pop(0)
-for i in b:
-    c = i.split(" ")
-    while '' in c:
-        c.remove('')
-    try:
-        print(
-            f"도커 컨테이너 이름 :: {c[0]}, 도커 컨테이너 ID :: {c[2]}")
-    except:
-        pass
+import locale
+import subprocess
+
+os_encoding = locale.getpreferredencoding()
+print(f"시스템 인코딩 :: {os_encoding}")
+cmd = "docker ps -a"
+po = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+decoded = po.stdout.read().decode('utf-8').strip()
+tables = decoded.split("\n")
+column = tables.pop(0)
+if tables:
+    for i in tables:
+        c = i.split(" ")
+        while '' in c:
+            c.remove('')
+        try:
+            print(f"컨테이너 이름 :: {c[-1]}, 컨테이너 ID :: {c[0]},  이미지 이름 :: {c[1]}")
+        except:
+            pass
+else:
+    print("이미지 없음")
